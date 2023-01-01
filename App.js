237502225fb4dec409e25dcf93040fb4fdc4a014
unknown <ibr.s.ahmat@gmail.com>
2023-01-01 03:08:03 +0300

    Used some React Native components and api

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, SafeAreaView, Alert, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button
+        color="orange"
+        title="Click me!"
+        onPress={() => Alert.prompt("My title", (text) => console.log(text))}
+      />
+    </SafeAreaView>
   );
 }
 
