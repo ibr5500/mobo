e32d67f8677d616250e949b9c9ec6280e00f30b6
unknown <ibr.s.ahmat@gmail.com>
2023-01-02 16:31:15 +0300

    Add delete and close styles

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,16 +1,41 @@
 import React from "react";
-import { Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 
 export default function ViewImageScreen() {
   return (
-    <Image
-      style={styles.image}
-      source={require("../../assets/chair.jpg")}
-    />
+    <View style={styles.container}>
+      <View style={styles.closeIcone}></View>
+      <View style={styles.deleteIcone}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../../assets/chair.jpg")}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+  },
+  closeIcone: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fc5c65",
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  deleteIcone: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#4ecdc4",
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
   image: {
     width: "100%",
     height: "100%",
